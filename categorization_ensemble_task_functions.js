2f39d8ac7236e3e5a93e97d07cad4329934718ba
Amit Goldenberg <amitgold@stanford.edu>
2019-04-03 13:46:25 -0700

    issue with check typing

--- categorization_ensemble_task_functions.js
+++ categorization_ensemble_task_functions.js
@@ -1,6 +1,41 @@
 /**
-Various useful functions
+Various gerneric useful functions
 **/
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random()*(max - min + 1) + min);
+}
+
+function getRandomElement (list){
+  return list[Math.floor(Math.random()*list.length)];
+}
+
+function getWord (){ //get a word for attention check from the word list
+  Face.word = Face.wordList.shift();
+  return Face.word;
+}
+
+/***
+  loading data and arranging
+  ***/
+function getNextSlide () {  //use to shift instruction slides
+  var currentSlide = slideList.shift();
+  return currentSlide;
+}
+
+
+function getFixationTime (){  //get randomized time of fixation by randomly choosing from 0.5, 1 and 1.5s
+  Face.fixationTime = getRandomElement([500, 1000, 1500]);
+  return Face.fixationTime;
+}
+
+function loadFacePool(start,end) { //the start and ending index of the images
+  var list = [];
+  for(i = start; i < (end+1); i++){
+     list.push( 'img/A' + i + '.jpg'); list.push( 'img/B' + i + '.jpg');
+     list.push( 'img/C' + i + '.jpg'); list.push( 'img/D' + i + '.jpg');}
+  return list;
+}
 
 function emotionValence(emotion){ //choose positive or negative valence
   if (emotion == 'positive'){
@@ -19,44 +54,9 @@ function loadStimulus(type,start,end) { //the start and ending index of the imag
     if (type == 'practice'){
       list.push( 'stimuli/practice/' + '1_0' + ("0" + i).slice(-2) + '.png');
     } else if (Face.emotionX == 50){
-    list.push( 'stimuli/positive/' + i + '.jpg');
-    } else {list.push( 'stimuli/negative/' + '1_0' + ('0' + i).slice(-2) + '.png');}}
-  return list;
-}
-
-function getWord (){ //get a word for attention check from the word list
-  Face.word = Face.wordList.shift();
-  return Face.word;
-}
-
-function getStimList(min1,max1,min2,max2) {  //min1:first index of practice stim, min1:first index of task stim
-  var stims = [];
-  for(i = min2; i < (max2+1); i++){    //use loop to get a list of stimulus with sequential numbers in file names
-      if (Face.emotionX == 50){
-             stims.push( 'stimuli/positive/' + i + '.jpg');
-      } else { stims.push( 'stimuli/negative/' + '1_0' + ("0" + i).slice(-2) + '.png')}};//add task stims
-  var stims = jsPsych.randomization.shuffle(stims);
-  for(i = min1; i < (max1+1); i++){    //use loop to get a list of stimulus with sequential numbers in file names
-      stims.push( 'stimuli/practice/' + '1_0' + ("0" + i).slice(-2) + '.png')};//add practice stims
-  return stims;  //attention please! in the list, 4 practice stimulus are AT TGE END (for convenience of shuffling and ordering)
-}
-
-function getFaceStimList() { //the start and ending index of the images
-    //choose positive or negative valence
-    Face.emotionX = getRandomElement([50, 100]); //randomly choose from negative and postive emotion
-    //choose the identity of the face
-    Face.personX = getRandomElement(['A','B','C','D']);//randomally choose from ['A','B','C','D'] -- select person
-    //define 16 faces in the face array
-    Face.stim = getRandomInt(1,50); //randomly select 16 faces
-    return 'img/'+ Face.personX +(Face.emotionX + Face.stim) + '.jpg' ;
-}
-
-
-function loadFacePool(start,end) { //the start and ending index of the images
-  var list = [];
-  for(i = start; i < (end+1); i++){
-     list.push( 'img/A' + i + '.jpg'); list.push( 'img/B' + i + '.jpg');
-     list.push( 'img/C' + i + '.jpg'); list.push( 'img/D' + i + '.jpg');}
+      list.push( 'stimuli/positive/' + i + '.jpg');
+    } else {
+      list.push( 'stimuli/negative/' + '1_0' + ('0' + i).slice(-2) + '.png');}}
   return list;
 }
 
@@ -67,15 +67,75 @@ function createSlideList(start,end){
   return list;
 }
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random()*(max - min + 1) + min);
+/**
+  functions that are related to the task at hand
+
+*/
+
+function getFaceStim() { //first stiumli presented to articipants
+    Face.personX = getRandomElement(['A','B','C','D']);//randomally choose from ['A','B','C','D'] -- select person
+    //define 16 faces in the face array
+    Face.stim = getRandomInt(1,50); //randomly select 16 faces
+    return 'img/'+ Face.personX +(Face.emotionX + Face.stim) + '.jpg' ;
 }
 
-function getRandomElement (list){
-  return list[Math.floor(Math.random()*list.length)];
+function getScale (){ //generate the rating scale depending on the person and valence randomly chosen in faceArray
+  //identify of the face is similar to the original ientity.
+  return ['img/'+
+    Face.personX+(Face.emotionX + 3*0) + '.jpg', 'img/'+Face.personX+(Face.emotionX + 3*1) + '.jpg', 'img/'+
+    Face.personX+(Face.emotionX + 3*2) + '.jpg', 'img/'+Face.personX+(Face.emotionX + 3*3) + '.jpg', 'img/'+
+    Face.personX+(Face.emotionX + 3*4) + '.jpg', 'img/'+Face.personX+(Face.emotionX + 3*5) + '.jpg', 'img/'+
+    Face.personX+(Face.emotionX + 3*6) + '.jpg', 'img/'+Face.personX+(Face.emotionX + 3*7) + '.jpg', 'img/'+
+    Face.personX+(Face.emotionX + 3*8) + '.jpg', 'img/'+Face.personX+(Face.emotionX + 3*9) + '.jpg', 'img/'+
+    Face.personX+(Face.emotionX + 3*10)+ '.jpg', 'img/'+Face.personX+(Face.emotionX + 3*11)+ '.jpg', 'img/'+
+    Face.personX+(Face.emotionX + 3*12)+ '.jpg', 'img/'+Face.personX+(Face.emotionX + 3*13)+ '.jpg', 'img/'+
+    Face.personX+(Face.emotionX + 3*14)+ '.jpg', 'img/'+Face.personX+(Face.emotionX + 3*15)+ '.jpg', 'img/'+
+    Face.personX+(Face.emotionX + 3*16)+ '.jpg', 'img/'+Face.personX+(Face.emotionX + 1*50)+ '.jpg']
 }
 
-function checkID (){
+function getFaceSample (){  //get the array  of faces in each trial based on the stimuli seen in hte beginning. notice that we want to miminize the range to 10-40 to get a distbution.
+  Face.sampleSD = getRandomElement([face5sd,face10sd]); //random select from SD=5 and SD=10,
+  Face.recordSD = Face.sampleSD[0];
+  //giving the conditions based on the samples we have which are 10-40
+  if ( Face.stim <= 10) {  //if you rated the picture between 10-20 you can only be assigned to the same or higher condition
+    Face.sampleMean = 10 + getRandomElement([0, +10]);
+  } else if ( Face.stim>10 && Face.stim < 20) {  //if you rated the picture between 10-20 you can only be assigned to the same or higher condition
+    Face.sampleMean = Face.stim + getRandomElement([0, +10]);
+  } else if ( Face.stim>=20 && Face.stim <=30) {
+    Face.sampleMean = Face.stim + getRandomElement([0, -10, +10]);
+  } else if (Face.stim > 30 && Face.stim<40) {   //If you rated the picture between 30 to 40 you can only be assigned to lower or same
+    Face.sampleMean = Face.stim + getRandomElement([0, -10]);
+  } else {
+    Face.sampleMean = 40 + getRandomElement([0, -10]);
+  }
+  Face.pool = (Face.sampleSD[1].responseJSON[Face.sampleMean]).slice(0, 12);//get an array of face index from JSON
+  Face.pos = jsPsych.randomization.shuffle(Face.pool); //randomize the 12 faces
+  return [
+    ['img/'+ Face.personX +(Face.emotionX + Face.pos[0] -100) + '.jpg', 'img/'+ Face.personX +(Face.emotionX + Face.pos[1]-100) + '.jpg',
+     'img/'+ Face.personX +(Face.emotionX + Face.pos[2] -100) + '.jpg', 'img/'+ Face.personX +(Face.emotionX + Face.pos[3]-100) + '.jpg'],
+    ['img/'+ Face.personX +(Face.emotionX + Face.pos[4] -100) + '.jpg', 'img/'+ Face.personX +(Face.emotionX + Face.pos[5]-100) + '.jpg',
+     'img/'+ Face.personX +(Face.emotionX + Face.pos[6] -100) + '.jpg', 'img/'+ Face.personX +(Face.emotionX + Face.pos[7]-100) + '.jpg'],
+    ['img/'+ Face.personX +(Face.emotionX + Face.pos[8] -100) + '.jpg', 'img/'+ Face.personX +(Face.emotionX + Face.pos[9]-100)+ '.jpg',
+     'img/'+ Face.personX +(Face.emotionX + Face.pos[10]-100)+ '.jpg', 'img/'+ Face.personX +(Face.emotionX + Face.pos[11]-100)+ '.jpg']
+    ];
+}
+
+function getButtons() { //my group or not my group button.
+  var trialButtons = [
+  '<button class="jspsych-btn" style="color:white; font-size: 20px; padding: 26px ;background-color:black; position: fixed; left:25%;top:36%; width: 210px;box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.9);border-radius: 50%">%choice%</button>',
+  '<button class="jspsych-btn" style="color:white; font-size: 20px; padding: 26px ;background-color:red;position: fixed; left:62%;top:36%;width: 210px;box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.9);border-radius: 50%">%choice%</button>'];
+  myButtons = [];
+  myButtons.push(trialButtons);
+  //alert (myButtons)
+  return myButtons[myButtons.length -1];
+}
+
+
+/*
+  functions that check participants attention and identity
+*/
+
+function checkID (){ //check if id is good
   var lasttrialdata = jsPsych.data.getLastTrialData().select('responses').values[0];
   var textInput = JSON.parse(lasttrialdata).Q0;
   var patt = new RegExp("^[a-zA-Z_0-9]{1,}$"); //the first and last character (this doesn't allow punctuations)
@@ -95,15 +155,6 @@ function checkUser (){//check if user has been in list of
   } else { return false;}
 }
 
-function checkAnswer (){
-  var inputText = jsPsych.data.getLastTrialData().select('responses').values[0];
-  var text = JSON.parse(inputText).Q0;
-  var patt = new RegExp("[A-Za-z0-9 _.,!'/$]"); // this allows punctuations
-  if (!patt.test(inputText  )){      //test if first/last character in response exist
-    alert("Please describe the image just showed in a few words (this will be uses for validation purposes)");
-    return true; }
-  else{ return false;}
-}
 
 function checkCitizen (){
   var choice = jsPsych.data.getLastTrialData().select('button_pressed').values[0];
@@ -132,44 +183,7 @@ var check_consent = function(elem) {
   } return false;
 }
 
-function getNextSlide () {  //use to shift instruction slides
-  var currentSlide = slideList.shift();
-  return currentSlide;
-}
 
-function getSortImage(){ //the image for minimal group paradigm
-  stim = '<img src=img/sort/'+ Object.keys(sortImage)[0] +'.jpg style="margin:30px">'+
-         '<img src=img/sort/'+ Object.keys(sortImage)[1]+'.jpg style="margin:30px">';
-  return stim
-}
-
-function getPrompt(){ //the prompt for minimal group paradigm
-  return sortPrompt.shift();
-}
-
-function getOptions(){   //the options for minimal group paradigm
-  return [Object.values(sortImage)[0], Object.values(sortImage)[1]]};
-
-function optionButton(){
-  var trialButtons = [
-    '<button class="jspsych-btn" style="font-size: 24px; padding: 10px ; position: fixed; left:29%;top:80%; width: 170px;box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.9);border-radius: 80%">%choice%</button>',
-    '<button class="jspsych-btn" style="font-size: 24px; padding: 10px ; position: fixed; left:62%;top:80%; width: 170px;box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.9);border-radius: 80%">%choice%</button>'
-    ];
-    myButtons = [];
-    myButtons.push(trialButtons);
-    //alert (myButtons)
-    return myButtons[myButtons.length -1];
-  }
-
-function getFixationTime (){  //get randomized time of fixation by randomly choosing from 0.5, 1 and 1.5s
-  Face.fixationTime = getRandomElement([500, 1000, 1500]);
-  return Face.fixationTime;
-}
-
-//function getStim (){
-//  Face.stim =  Face.stims.pop();
-//  return Face.stim //get last stim of the stim list
-//}
 function checkTyping(){  //test if type correctly
   var inputText = jsPsych.data.getLastTrialData().select('responses').values[0];
   var text = JSON.parse(inputText).Q0;
@@ -184,54 +198,60 @@ function checkTyping(){  //test if type correctly
   else {falseAnswer = 0; return false} //reset falseAnswer
 }
 
-
-function getScale (){ //generate the rating scale depending on the person and valence randomly chosen in faceArray
-  //identify of the face is similar to the original ientity.
-  //Face.personX = getRandomElement(['A','B','C','D']);//randomally choose from ['A','B','C','D'] -- select person
-  return ['img/'+
-    Face.personX+(Face.emotionX + 3*0) + '.jpg', 'img/'+Face.personX+(Face.emotionX + 3*1) + '.jpg', 'img/'+
-    Face.personX+(Face.emotionX + 3*2) + '.jpg', 'img/'+Face.personX+(Face.emotionX + 3*3) + '.jpg', 'img/'+
-    Face.personX+(Face.emotionX + 3*4) + '.jpg', 'img/'+Face.personX+(Face.emotionX + 3*5) + '.jpg', 'img/'+
-    Face.personX+(Face.emotionX + 3*6) + '.jpg', 'img/'+Face.personX+(Face.emotionX + 3*7) + '.jpg', 'img/'+
-    Face.personX+(Face.emotionX + 3*8) + '.jpg', 'img/'+Face.personX+(Face.emotionX + 3*9) + '.jpg', 'img/'+
-    Face.personX+(Face.emotionX + 3*10)+ '.jpg', 'img/'+Face.personX+(Face.emotionX + 3*11)+ '.jpg', 'img/'+
-    Face.personX+(Face.emotionX + 3*12)+ '.jpg', 'img/'+Face.personX+(Face.emotionX + 3*13)+ '.jpg', 'img/'+
-    Face.personX+(Face.emotionX + 3*14)+ '.jpg', 'img/'+Face.personX+(Face.emotionX + 3*15)+ '.jpg', 'img/'+
-    Face.personX+(Face.emotionX + 3*16)+ '.jpg', 'img/'+Face.personX+(Face.emotionX + 1*50)+ '.jpg']
+/*
+unsed fucntions that may be useful
+*/
+/*
+function getStim (){
+  Face.stim =  Face.stims.pop();
+  return Face.stim //get last stim of the stim list
 }
 
-
-function getFaceSample (){  //get the sample of faces in each trial
-  Face.sampleSD = getRandomElement([face5sd,face10sd]); //random select from SD=5 and SD=10,
-  Face.recordSD = Face.sampleSD[0];
-
-  if ( Face.stim <= 10) {  //if you rated the picture between 10-20 you can only be assigned to the same or higher condition
-    Face.sampleMean = 10 + getRandomElement([0, +10]);
-  } else if ( Face.stim>10 && Face.stim < 21) {  //if you rated the picture between 10-20 you can only be assigned to the same or higher condition
-    Face.sampleMean = Face.stim + getRandomElement([0, +10]);
-  } else if (Face.stim > 30 && Face.stim<40) {   //If you rated the picture between 30 to 40 you can only be assigned to lower or same
-    Face.sampleMean = Face.stim + getRandomElement([0, -10]);
-  } else {
-    Face.sampleMean = 40 + getRandomElement([0, -10, +10]);}
-  Face.pool = (Face.sampleSD[1].responseJSON[Face.sampleMean]).slice(0, 12);//get an array of face index from JSON
-  Face.pos = jsPsych.randomization.shuffle(Face.pool); //randomize the 12 faces
-
-  return [
-    ['img/'+ Face.personX +(Face.emotionX + Face.pos[0] -100) + '.jpg', 'img/'+ Face.personX +(Face.emotionX + Face.pos[1]-100) + '.jpg',
-     'img/'+ Face.personX +(Face.emotionX + Face.pos[2] -100) + '.jpg', 'img/'+ Face.personX +(Face.emotionX + Face.pos[3]-100) + '.jpg'],
-    ['img/'+ Face.personX +(Face.emotionX + Face.pos[4] -100) + '.jpg', 'img/'+ Face.personX +(Face.emotionX + Face.pos[5]-100) + '.jpg',
-     'img/'+ Face.personX +(Face.emotionX + Face.pos[6] -100) + '.jpg', 'img/'+ Face.personX +(Face.emotionX + Face.pos[7]-100) + '.jpg'],
-    ['img/'+ Face.personX +(Face.emotionX + Face.pos[8] -100) + '.jpg', 'img/'+ Face.personX +(Face.emotionX + Face.pos[9]-100)+ '.jpg',
-     'img/'+ Face.personX +(Face.emotionX + Face.pos[10]-100)+ '.jpg', 'img/'+ Face.personX +(Face.emotionX + Face.pos[11]-100)+ '.jpg']
-    ];
+function getPrompt(){ //the prompt for minimal group paradigm
+  return sortPrompt.shift();
 }
+function getSortImage(){ //the image for minimal group paradigm
+  stim = '<img src=img/sort/'+ Object.keys(sortImage)[0] +'.jpg style="margin:30px">'+
+         '<img src=img/sort/'+ Object.keys(sortImage)[1]+'.jpg style="margin:30px">';
+  return stim
+}
+function getOptions(){   //the options for minimal group paradigm
+  return [Object.values(sortImage)[0], Object.values(sortImage)[1]]};
 
-function getButtons() {
+function optionButton(){
   var trialButtons = [
-  '<button class="jspsych-btn" style="color:white; font-size: 20px; padding: 26px ;background-color:black; position: fixed; left:25%;top:36%; width: 210px;box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.9);border-radius: 50%">%choice%</button>',
-  '<button class="jspsych-btn" style="color:white; font-size: 20px; padding: 26px ;background-color:red;position: fixed; left:62%;top:36%;width: 210px;box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.9);border-radius: 50%">%choice%</button>'];
-  myButtons = [];
-  myButtons.push(trialButtons);
-  //alert (myButtons)
-  return myButtons[myButtons.length -1];
+    '<button class="jspsych-btn" style="font-size: 24px; padding: 10px ; position: fixed; left:29%;top:80%; width: 170px;box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.9);border-radius: 80%">%choice%</button>',
+    '<button class="jspsych-btn" style="font-size: 24px; padding: 10px ; position: fixed; left:62%;top:80%; width: 170px;box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.9);border-radius: 80%">%choice%</button>'
+    ];
+    myButtons = [];
+    myButtons.push(trialButtons);
+    //alert (myButtons)
+    return myButtons[myButtons.length -1];
+  }
+
+function getStimList(min1,max1,min2,max2) {  //min1:first index of practice stim, min1:first index of task stim
+  var stims = [];
+  for(i = min2; i < (max2+1); i++){    //use loop to get a list of stimulus with sequential numbers in file names
+      if (Face.emotionX == 50){
+         stims.push( 'stimuli/positive/' + i + '.jpg');
+      } else {
+        stims.push( 'stimuli/negative/' + '1_0' + ("0" + i).slice(-2) + '.png')}};//add task stims
+  var stims = jsPsych.randomization.shuffle(stims);
+  for(i = min1; i < (max1+1); i++){    //use loop to get a list of stimulus with sequential numbers in file names
+      stims.push( 'stimuli/practice/' + '1_0' + ("0" + i).slice(-2) + '.png')};//add practice stims
+  return stims;  //attention please! in the list, 4 practice stimulus are AT TGE END (for convenience of shuffling and ordering)
 }
+
+function checkAnswer (){
+  var inputText = jsPsych.data.getLastTrialData().select('responses').values[0];
+  var text = JSON.parse(inputText).Q0;
+  var patt = new RegExp("[A-Za-z0-9 _.,!'/$]"); // this allows punctuations
+  if (!patt.test(inputText  )){      //test if first/last character in response exist
+    alert("Please describe the image just showed in a few words (this will be uses for validation purposes)");
+    return true; }
+  else{ return false;}
+}
+
+
+
+*/
